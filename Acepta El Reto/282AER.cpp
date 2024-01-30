c4074710011edefb5ed50f774eaeb6816975d3c5
lucasmg18 <margarlucas@gmail.com>
2024-01-30 14:01:41 +0100

    New solutions to Acepta EL Reto

---
+++ Acepta El Reto/282AER.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+bool resuelveCaso() {
+	int N, A;
+	cin >> N >> A;
+	if (!cin) return false;
+	vector<int> aguj(N);
+	for (int i = 0; i < N; i++) {
+		cin >> aguj[i];
+	}
+	int cont = 0, agAct = 0, posAct;
+	while (agAct < N) {
+		posAct = aguj[agAct];
+		while (agAct < N &&aguj[agAct] <= posAct + A) {
+			agAct++;
+		}
+		cont++;
+	}
+	cout << cont << endl;
+
+
+	return true;
+}
+
+int main() {
+	while (resuelveCaso()) {
+
+	}
+}
---
+++ Acepta El Reto/329AER.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+void resuelveCaso(int N) {
+	vector<pair<int,int>> edif(N);
+	for (int i = 0; i < N; i++) {
+		cin >> edif[i].second >> edif[i].first;
+	}
+	sort(edif.begin(), edif.end());
+
+	int cont = 0, act = 0, ant;
+	while (act < N) {
+		ant = act;
+		while (act < N && edif[act].second < edif[ant].first) {
+			act++;
+		}
+		cont++;
+	}
+	cout << cont << endl;
+}
+
+int main() {
+	int N;
+	cin >> N;
+	while (N != 0) {
+		resuelveCaso(N);
+		cin >> N;
+	}
+}
